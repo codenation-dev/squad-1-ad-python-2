7dd0c82882a7c9ae7a0c115cb9d662aae067424b
Eduardo <lodi001@uol.com.br>
2019-07-18 14:48:33 -0300

    vai

--- comission/api/models.py
+++ comission/api/models.py
@@ -7,17 +7,17 @@ class Sellers(models.Model):
     age = models.IntegerField()
     email = models.EmailField(max_length=100)
     cpf = models.IntegerField()
-    comission_plan = models.IntegerField()
+    plan = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Comission_plan(models.Model):
-    plan = models.CharField(max_length=100)
-    sellers = models.OneToOneField(Sellers, on_delete=models.CASCADE, primary_key=False, related_name="selec")
-    lower_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    upper_percentage = models.DecimalField(max_digits=5, decimal_places=2)
-    min_value = models.DecimalField(max_digits=19, decimal_places=2)
+    comission_plan = models.IntegerField()
+    sellers_id = models.ForeignKey(Sellers, on_delete=models.CASCADE, primary_key=False, verbose_name="sid")
+    lower_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="lo")
+    upper_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="up")
+    min_value = models.DecimalField(max_digits=19, decimal_places=2,verbose_name="min")
 
     def __srt__(self):
         return "%s" % self.plan
@@ -25,14 +25,13 @@ class Comission_plan(models.Model):
 class Sales(models.Model):
     month = models.IntegerField()
     amount = models.DecimalField(max_digits=20, decimal_places=2)
-    sellers = models.ForeignKey(Sellers, on_delete=models.CASCADE, primary_key=False)
+    sellers_id = models.ForeignKey(Sellers, on_delete=models.CASCADE, primary_key=False, verbose_name="si")
     comission = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
         return "%s" % self.sellers
     
     def calc_comission(self, sellers, amount, month):
-        c = Sales(month=month, amount=amount, sellers=sellers)
         if c.amount <= sellers.comission_plan.min_value:
             return c.amount * sellers.comission_plan.lower_percentage / 100
         else:
