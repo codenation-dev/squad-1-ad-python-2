d92e171283e8c68c53074acd4827cfaa558b1620
Eduardo <lodi001@uol.com.br>
2019-07-20 22:02:58 -0300

    Criado o método return_sellers

--- comission/api/models.py
+++ comission/api/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from decimal import Decimal
 
 
+class MyDecimal(Decimal):
+    def __repr__(self):
+        return str(float(self))
+
 class Comission_plan(models.Model):
     lower_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="lo")
     upper_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="up")
     min_value = models.DecimalField(max_digits=19, decimal_places=2,verbose_name="min")
 
     def __srt__(self):
-        return "Plano: %s" % self.id
+        return self.id
 
 class Sellers(models.Model):
     name = models.CharField(max_length=100)
@@ -31,15 +35,15 @@ class Sales(models.Model):
     comission = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
-        return "%s" % self.sellers_id.name
+        return "Seller: %s" % self.sellers_id.name
     
     def calc_comission(self, seller, amount):
         sel_calc = Sellers.objects.get(id=seller)
-        if amount <= sel_calc.plan.min_value:
-            comission = amount * sel_calc.plan.lower_percentage / 100
+        if Decimal(amount) <= Decimal(sel_calc.plan.min_value):
+            comission = Decimal(amount) * Decimal(sel_calc.plan.lower_percentage / 100)
         else:
-            comission = amount * sel_calc.plan.upper_percentage / 100
-        return round(comission, 2)
+            comission = Decimal(amount) * Decimal(sel_calc.plan.upper_percentage / 100)
+        return round(Decimal(comission), 2)
     
     def sales_month(self, seller, amount, month):
         sel_month = Sellers.object.get(id=seller)
@@ -47,4 +51,8 @@ class Sales(models.Model):
         s = Sales(sellers_id=sel_month, amount=month_amount, month=month)
         s.comission = self.calc_comission(seller, month_amount)
         s.save()
-        return s.id, s.comission
+        return s.id, MyDecimal(s.comission)
+
+    def return_sellers(self, month):
+        return sorted([{"name": i.sellers_id.name, "id": i.sellers_id.id, "comission": MyDecimal(i.comission)} for i in 
+                        Sales.objects.filter(month=month)], key=lambda x: x['comission'], reverse=True)
