3f11d8bff3cff5baf0ba8a61c950e5e97062b574
Eduardo <lodi001@uol.com.br>
2019-07-23 19:40:00 -0300

    Delete admin.py

--- comission/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Sellers, Comission_plan, Sales
-
-admin.site.register(Sellers)
-admin.site.register(Comission_plan)
-admin.site.register(Sales)
