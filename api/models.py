b923e9ac46293f6417131a6310edb599cb8cf396
Matheus Amelco <matheusamelco@hotmail.com>
2019-07-28 15:30:45 -0300

    Aplicação de JWT e ajustes nos Models

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from decimal import Decimal
 import datetime
-import json
-from operator import itemgetter as ig
 from django.core.mail import send_mail
 
 
@@ -12,12 +10,9 @@ class MyDecimal(Decimal):
 
 
 class Commission_plan(models.Model):
-    lower_percentage = models.DecimalField(
-        max_digits=5, decimal_places=2, verbose_name="lo")
-    upper_percentage = models.DecimalField(
-        max_digits=5, decimal_places=2, verbose_name="up")
-    min_value = models.DecimalField(
-        max_digits=19, decimal_places=2, verbose_name="min")
+    lower_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="lo")
+    upper_percentage = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="up")
+    min_value = models.DecimalField(max_digits=19, decimal_places=2, verbose_name="min")
 
     def __srt__(self):
         return self.id
@@ -30,18 +25,31 @@ class Sellers(models.Model):
     age = models.IntegerField()
     email = models.EmailField(max_length=100)
     cpf = models.CharField(max_length=11)
-    plan = models.ForeignKey(
-        Commission_plan, on_delete=models.CASCADE, verbose_name="plan")
+    plan = models.ForeignKey(Commission_plan, on_delete=models.CASCADE, verbose_name="plan")
 
     def __str__(self):
         return self.name
 
 
 class Sales(models.Model):
-    month = models.IntegerField()
+    MONTHS = (
+        (1, 'Janeiro'),
+        (2, 'Fevereiro'),
+        (3, 'Março'),
+        (4, 'Abril'),
+        (5, 'Maio'),
+        (6, 'Junho'),
+        (7, 'Julho'),
+        (8, 'Agosto'),
+        (9, 'Setembro'),
+        (10, 'Outubro'),
+        (11, 'Novembro'),
+        (12, 'Dezembro'),
+    )
+
+    month = models.IntegerField(choices=MONTHS)
     amount = models.DecimalField(max_digits=20, decimal_places=2)
-    sellers_id = models.ForeignKey(
-        Sellers, on_delete=models.CASCADE, primary_key=False, verbose_name="sid")
+    sellers_id = models.ForeignKey(Sellers, on_delete=models.CASCADE, primary_key=False, verbose_name="sid")
     commission = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
@@ -54,11 +62,9 @@ class Sales(models.Model):
             sel_calc = Sellers.objects.get(id=seller)
             dec_amount = MyDecimal(amount)
             if dec_amount <= MyDecimal(sel_calc.plan.min_value):
-                commission = dec_amount * \
-                    MyDecimal(sel_calc.plan.lower_percentage / 100)
+                commission = dec_amount * MyDecimal(sel_calc.plan.lower_percentage / 100)
             else:
-                commission = dec_amount * \
-                    MyDecimal(sel_calc.plan.upper_percentage / 100)
+                commission = dec_amount * MyDecimal(sel_calc.plan.upper_percentage / 100)
         return MyDecimal(round(commission, 2))
 
     def sales_month(self, seller, amount, month):
@@ -69,6 +75,7 @@ class Sales(models.Model):
             s.commission = self.calc_commission(seller, month_amount)
             s.save()
             return {"id": s.id, "commission": MyDecimal(s.commission)}
+
         else:
             return "Venda já cadastrada para este mês"
 
@@ -88,22 +95,20 @@ class Sales(models.Model):
                 'Notificação - valor de vendas',
                 'Suas vendas no mês estão abaixo da média mensal.',
                 'commission_admin@mail.com',
-                [Sales.objects.get(sellers_id=seller,
-                                   month=month).sellers_id.email],
+                [Sales.objects.get(sellers_id=seller, month=month).sellers_id.email],
                 fail_silently=False
             )
 
     def check_commission(self, seller, amount):
         month = datetime.datetime.now().month
-        db_fetch = Sales.objects.select_related(
-            'sellers_id').filter(sellers_id=seller)
+        db_fetch = Sales.objects.select_related('sellers_id').filter(sellers_id=seller)
         if not db_fetch:
             return "Vendedor não cadastrado. Favor verificar os dados de entrada."
         else:
             for i in range(len(db_fetch)):
                 if db_fetch[i].month == month:
                     db_fetch[i].amount = MyDecimal(amount)
-                    db_fetch[i].commission = calc_commission(seller, amount)
+                    db_fetch[i].commission = self.calc_commission(seller, amount)
                 db_fetch.order_by('-commission')
             avg_sales = (sum([db_fetch[i].amount * (len(db_fetch) - i) for i in range(len(db_fetch))]) /
                          sum([len(db_fetch) - i for i in range(len(db_fetch))]))
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from api import views
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 
 urlpatterns = [
     path('sellers/', views.sellers),
@@ -7,4 +9,6 @@ urlpatterns = [
     path('month_commission/', views.month_commission),
     path('vendedores/<int:month>/', views.vendedores),
     path('check_commission/', views.check_commission),
+    path('auth/', obtain_jwt_token),
+    path('auth/refresh-token/', refresh_jwt_token),
 ]
